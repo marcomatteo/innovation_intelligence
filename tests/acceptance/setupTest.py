67f84883e3532061521d2921c5076e3981549cfa
marcomatteo <49158951+marcomatteo@users.noreply.github.com>
2020-11-11 15:16:20 +0100

    fix2

--- tests/acceptance/setupTest.py
+++ tests/acceptance/setupTest.py
@@ -1,6 +1,9 @@
 from sys import path
 from pathlib import Path
+
 wrkdir = Path().absolute()
 if not list(filter(lambda x: "innovation_intelligence" in x, path)):
     # innovation_intelligence dir (path/../..)
     path.append(wrkdir.parents[1])
+
+print(path)
